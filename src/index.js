e80051760dd4303b1a4dc569b64916dacece8fd3
Moin <moinpatel1992@gmail.com>
2019-09-29 16:20:14 -0400

    Wesbos react course follow up

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Componnent{
+    render() {
+        return <p>Hello!</p>
+    }
+}
+render(<p>heyyyy</p>, document.querySelector('#main'));
